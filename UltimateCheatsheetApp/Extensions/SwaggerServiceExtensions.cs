a67549f567198d7aa963f4a424881ad5cce836d7
Denys Fostytskyi <denys.fostytskyi@outlook.com>
2024-11-26 17:59:11 +0200

    Configured swagger service to use bearer authorization

---
+++ UltimateCheatsheetApp/Extensions/SwaggerServiceExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace UltimateCheatsheetApp.Extensions
+{
+    public static class SwaggerServiceExtensions
+    {
+        public static IServiceCollection AddSwaggerService(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+
+            //Adding security definition for Bearer tokens
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Cheatsheet API", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme.",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                    Array.Empty<string>()
+                    }
+                });
+            });
+
+            return services;
+        }
+    }
+}
